81ee8894a6731af0ccdd590ea8b70484e831ea67
Marty Rath <martydavidger@gmail.com>
2024-02-18 10:25:25 +0000

    Added about view, controller, and route.

--- src/web-routes.js
+++ src/web-routes.js
@@ -1,8 +1,10 @@
 import { accountsController } from "./controllers/accounts-controller.js";
 import { dashboardController } from "./controllers/dashboard-controller.js";
+import { aboutController } from "./controllers/about-controller.js";
 
 export const webRoutes = [
   { method: "GET", path: "/", config: accountsController.index },
+  { method: "GET", path: "/about", config: aboutController.index },
   { method: "GET", path: "/signup", config: accountsController.showSignup },
   { method: "GET", path: "/login", config: accountsController.showLogin },
   { method: "GET", path: "/logout", config: accountsController.logout },
@@ -10,5 +12,4 @@ export const webRoutes = [
   { method: "POST", path: "/authenticate", config: accountsController.login },
 
   { method: "GET", path: "/dashboard", config: dashboardController.index },
-  { method: "POST", path: "/dashboard/addplaylist", config: dashboardController.addPlaylist },
 ];
