473987370ea7ac7f23fc5b696b05f35cce6ca401
Marty Rath <martydavidger@gmail.com>
2024-03-07 14:23:34 +0000

    Added comments to userTree and fixed ref

--- src/models/mongo/userTree.js
+++ src/models/mongo/userTree.js
@@ -8,11 +8,11 @@ const userTreeSchema = new Schema({
   height: Number,
   girth: Number,
   description: String,
-  province: String,
+  province: String,  // As provinces are simple strings, will not use ref from Province
   // Added userid here rather than in province
   userid: {
-    type: Schema.Types.ObjectId,
-    ref: "userid",
+    type: Schema.Types.ObjectId, // Refers to object in other collection
+    ref: "User", // Refers to object type: User
   },
 });
 
