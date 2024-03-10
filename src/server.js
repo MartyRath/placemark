f2c4b2de651be3be238d7758eb0f13b07cb3fb35
Marty Rath <martydavidger@gmail.com>
2024-03-10 12:29:32 +0000

    Added cloud atlas database

--- src/server.js
+++ src/server.js
@@ -30,6 +30,15 @@ const swaggerOptions = {
     title: "Playtime API",
     version: "0.1",
   },
+  // Adding authorise option to documentation
+  securityDefinitions: {
+    jwt: {
+      type: "apiKey",
+      name: "Authorization",
+      in: "header"
+    }
+  },
+  security: [{ jwt: [] }]
 };
 
 async function init() {
